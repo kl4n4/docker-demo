3b2c86128ea8ed09ff37c0db50f58a7b39fde6b0
Thomas Klaner <thomas@iwa-tech.com>
2019-02-17 11:37:10 +0100

    use docker secrets

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
+import * as fs from 'fs';
 import * as restify from 'restify';
 import { getArticles } from './NewsAPI';
 
 const server = restify.createServer({
   name: 'docker-demo',
-  version: '0.4.0',
+  version: '0.5.0',
 });
 
 server.get('/', async (req, res, next) => {
   const username = req.header('X-Consumer-Username', 'unknown');
   res.send(200, {
     message: `Hello ${username}! This is updatemi/docker-demo`,
-    articles: await getArticles(process.env.API_KEY),
+    articles: await getArticles(fs.readFileSync(process.env.API_SECRET).toString()),
   });
   next();
 });
